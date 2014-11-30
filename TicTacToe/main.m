43965c2273e92e2c9921d78f750d42360f5ed7bd
Nelly Santoso <nellysantoso@Ardians-MacBook-Pro.local>
2014-11-29 19:09:01 -0500

    Initial Commit

---
+++ TicTacToe/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TicTacToe
+//
+//  Created by Nelly Santoso on 11/29/14.
+//  Copyright (c) 2014 Nelly Santoso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
