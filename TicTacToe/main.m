43c0324567631fd976f2f317a3c5ce452ad7f501
Nelly Santoso <nellysantoso@Ardians-MacBook-Pro.local>
2014-11-30 21:10:05 -0500

    Adding player two for happy path case

--- TicTacToe/main.m
+++ TicTacToe/main.m
@@ -9,16 +9,18 @@
 #import <Foundation/Foundation.h>
 
 NSString* get_player_name(NSString *icon_name, NSNumber *player_number);
-NSString* player1_moves(NSString *player1_name);
+NSString* player_moves(NSString *player_name);
 //NSNumber* is_accepted_move(NSString *moves);
 NSMutableArray* processing_move(NSString *user_moves);
 //NSString* asking_again(NSString *player_name, NSString *user_moves);
-BOOL is_player_winning(NSArray *player_data);
+BOOL is_player_winning(NSArray *player_data, NSString *player_name);
+//void print_table(NSMutableDictionary *table_score, NSString *icon, NSString *player_moves);
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        NSDictionary *default_table = @{@1:@"A",@2:@"B",@3:@"C",@4:@"D",@5:@"E",@6:@"F",@7:@"G",@8:@"H",@9:@"I"};
+//        NSDictionary *default_table = @{@"A":@"A",@"B":@"B",@"C":@"C",@"D":@"D",@"E":@"E",@"F":@"F",@"G":@"G",@"H":@"H",@"I":@"I"};
 //        NSMutableDictionary *table_score = [NSMutableDictionary dictionaryWithDictionary:default_table];
         
 
@@ -33,7 +35,7 @@ int main(int argc, const char * argv[]) {
         
         // finding out the name of player one and player two
         NSString *player1_name = get_player_name(@"O",@1);
-//        NSString *player2_name = get_player_name(@"X",@2);
+        NSString *player2_name = get_player_name(@"X",@2);
 
         
         printf("Let's play tic tac toe game!\n");
@@ -42,19 +44,36 @@ int main(int argc, const char * argv[]) {
         printf("-------------\n| A | B | C |\n-------------\n| D | E | F |\n-------------\n| G | H | I |\n-------------\n");
         
         NSMutableArray *store_player1_data = [[NSMutableArray alloc] init];
-        
+        NSMutableArray *store_player2_data = [[NSMutableArray alloc] init];
 
         BOOL win_lose_status = NO;
         int ct;
         while(!win_lose_status && ct < 5)
         {
-            NSString *my_moves = player1_moves(player1_name);
-            NSString * good_data = [my_moves stringByReplacingOccurrencesOfString:@"\n" withString:@""];
-            [store_player1_data addObject:good_data];
+            NSLog(@"%hhd", win_lose_status);
+            // player 1
+            NSString *player1_moves = player_moves(player1_name);
+            NSString * player1_data = [player1_moves stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+            [store_player1_data addObject:player1_data];
             NSLog(@"%@",store_player1_data);
-            win_lose_status = is_player_winning(store_player1_data);
+//            print_table(table_score, @"O", player1_data);
+            win_lose_status = is_player_winning(store_player1_data, player1_name);
+            
+            if(win_lose_status) break;
+            
+            // player 2
+            NSString *player2_moves = player_moves(player2_name);
+            NSString *player2_data = [player2_moves stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+            [store_player2_data addObject:player2_data];
+            NSLog(@"%@",store_player2_data);
+//            print_table(table_score, @"X", player2_data);
+            win_lose_status = is_player_winning(store_player2_data, player2_name);
+            
+            if(win_lose_status) break;
             
             ct++;
+            
+            
         }
         
         
@@ -77,21 +96,23 @@ NSString* get_player_name(NSString *icon_name, NSNumber *player_number)
     return str_player;
 }
 
-NSString* player1_moves(NSString *player1_name)
+NSString* player_moves(NSString *player_name)
 {
-    printf("it's your turn, %s",[player1_name UTF8String]);
-    NSFileHandle *player1_data = [NSFileHandle fileHandleWithStandardInput];
-    NSData *player1_moves = [player1_data availableData];
-    NSString *move_player1 = [[NSString alloc] initWithData:player1_moves encoding:NSUTF8StringEncoding];
-    move_player1 = [move_player1 uppercaseString];
-    NSLog(@"%@", move_player1);
+    printf("it's your turn, %s",[player_name UTF8String]);
+    NSFileHandle *player_data = [NSFileHandle fileHandleWithStandardInput];
+    NSData *player_moves = [player_data availableData];
+    NSString *move_player = [[NSString alloc] initWithData:player_moves encoding:NSUTF8StringEncoding];
+    move_player = [move_player uppercaseString];
+    NSLog(@"%@", move_player);
     
-    return move_player1;
+    return move_player;
 }
 
-BOOL is_player_winning(NSArray *player_data)
+BOOL is_player_winning(NSArray *player_data, NSString *player_name)
 {
-    NSArray *winning=@[@[@"A",@"B",@"C"],@[@"A", @"D", @"G"],@[@"G",@"H",@"I"],@[@"C", @"F", @"I"],@[@"A",@"E",@"I"],@[@"C", @"E", @"G"]];
+    NSArray *winning=@[@[@"A",@"B",@"C"],@[@"A", @"D", @"G"],@[@"G",@"H",@"I"],
+                       @[@"C", @"F", @"I"],@[@"A",@"E",@"I"],@[@"C", @"E", @"G"],
+                       @[@"D", @"E", @"F"]];
     BOOL is_winning = NO;
     
     NSUInteger array_count = [player_data count];
@@ -99,13 +120,13 @@ BOOL is_player_winning(NSArray *player_data)
     
     
     if ([player_data count] > 2){
-        for ( int i = 0; i < 2; i++ )
+        for ( int i = 0; i < 7; i++ )
         {
             
             if ([[NSSet setWithArray:winning[i]] isSubsetOfSet:[NSSet setWithArray:player_data]])
             {
                 is_winning = YES;
-                NSLog(@"Congratulation! You Won!");
+                NSLog(@"Congratulation %@ !  You Won!", player_name);
                 NSLog(@"%@", winning[i]);
             }
             
@@ -113,6 +134,12 @@ BOOL is_player_winning(NSArray *player_data)
     }
     return is_winning;
 }
+
+//void print_table(NSMutableDictionary *table_score, NSString *icon, NSString *player_moves)
+//{
+//    [table_score setObject:icon forKey:player_moves];
+//    NSLog();
+//}
 
 //NSNumber* is_accepted_move(NSString *moves)
 //{
